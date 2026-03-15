f27447c06dd5cd0ceec422cc03741d1f78867f5b
abhishek.kale <abhisahebpatil@gmail.com>
2026-03-15 09:48:46 +0530

    completed-apply-form-controls

---
+++ src/app/enum/collegeyear.ts
@@ -0,0 +1,7 @@
+export enum CollegeYear {
+    FirstYear = 'First Year',
+    SecondYear = 'Second Year',
+    ThirdYear = 'Third Year',
+    FourthYear = 'Fourth Year',
+    FifthYear = 'fifth year'
+}
--- src/app/model/collegedetail.ts
+++ src/app/model/collegedetail.ts
@@ -1,8 +1,10 @@
+import { CollegeYear } from "../enum/collegeyear";
+
 export interface CollegeDetail {
     collegename: string;
     collegeDepartment: string;
     admissionDate: Date;
-    collegeCurrentYear: Date;
+    collegeCurrentYear: CollegeYear;
     percentage: number;
     isActive: boolean;
     skills: string[];
